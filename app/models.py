ea150cc07ad8aa3ce012a0392d3b2f47a7841fe3
bluesky <qtijay@126.com>
2018-09-19 15:33:04 +0800

    Mail Warning added. Logging File added. 404, 500, Error page added.

--- app/models.py
+++ app/models.py
@@ -5,9 +5,11 @@ from flask_login import UserMixin
 from app import login
 from hashlib import md5
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,6 +32,7 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,10 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 from app import db
-from app.forms import RegistrationForm
 from datetime import datetime
 
 @app.route('/')
@@ -26,6 +25,13 @@ def index():
     return render_template('index.html', title='Home Page', posts=posts)
 
 
+'''
+@app.route('/test')
+def test():
+    return render_template('test.html')
+'''
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -42,7 +48,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return  redirect(next_page)
+        return redirect(next_page)
 #        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
@@ -81,7 +87,7 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
         {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'},
+        {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
 
@@ -93,6 +99,19 @@ def before_request():
         db.session.commit()
 
 
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 
 
